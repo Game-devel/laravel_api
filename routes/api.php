b897c6f5e8f1e8d3eaf191277876e273fe3c93f3
game-devel1 <rahatsadykov784@gmail.com>
2020-08-11 21:49:37 +0600

    login api done

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('/v1/user')->group(function() {
+    Route::post('/login', 'api\v1\LoginController@login');    
 });
